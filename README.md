e4259be36d563c364cd7d19c07c5b97b46660a2f
lem0nb <68461894+lem0nb@users.noreply.github.com>
2023-02-01 01:01:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minecraft CDN
+Minecraft CDN
+调用方法：https://cdn.jsdelivr.net/gh/minecraftcdn/file/
---
+++ status/js/config.js
@@ -0,0 +1,7 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'ur1003269-a42a115868043ceea5bcf55a', 
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
